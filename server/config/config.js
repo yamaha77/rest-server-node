e1f9ea307c3d5109a300ca918a0ffc4f70bab0ec
JJ Quispe <joelquispeunju@gmail.com>
2019-05-15 00:42:46 -0300

    Modified environment variable

--- server/config/config.js
+++ server/config/config.js
@@ -9,6 +9,6 @@ let url_db;
 if ( process.env.NODE_ENV === 'dev' ) {
     url_db = 'mongodb://192.168.1.21:26017/coffee';
 } else {
-    url_db = 'mongodb+srv://yamaha6297:GwJTHWwy8Hoppr9L@cluster0-jz8da.mongodb.net/test'
+    url_db = process.env.MONGO_URI;
 }
 process.env.URL_DB = url_db;
